81a3afbaaa94bf534d44df5a279c4682d18a2133
ironman-tetsuo <58040608+ironman-tetsuo@users.noreply.github.com>
2025-01-01 07:45:24 +0100

    Create samtools_addreplacerg.sh

---
+++ scripts/samtools_addreplacerg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SampleNames=(`tail -n +2 metafiles/medaka_pheno14_Kon-add-wild-samples.tsv | awk -F "\t" 'BEGIN{OFS="\t"}{print $6}'  | xargs`)
+SampleNames=
+(
+sample1
+sample2
+sample3
+sample4
+sample5
+)
+
+bam_path="/path_to_bam_files"
+thread=40
+
+for i in `seq 0 $((${#SampleNames[@]}-1))`; do
+MyRG="@RG\tID:"${SampleNames[${i}]}"\tPL:ILLUMINA\tSM:"${SampleNames[${i}]}
+echo "Processing: ${SampleNames[${i}]}.sort.bam"
+samtools addreplacerg -r  ${MyRG} ${bam_path}/${SampleNames[${i}]}.sort.bam -O BAM -@ ${thread} > ${SampleNames[${i}]}.sort.bam
+samtools index ${SampleNames[${i}]}.sort.bam
+done
